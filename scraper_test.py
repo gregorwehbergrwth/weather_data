0dc0a40ec2f6d659551301ad279e687795139b05
gregorwehbergrwth <162116009+gregorwehbergrwth@users.noreply.github.com>
2024-10-28 20:45:00 +0100

    first test

--- scraper_test.py
+++ scraper_test.py
@@ -13,10 +13,19 @@ def get_page_data(html):
     h2 = soup.find('h2').text
     return h2
 
+def get_more_page_data(html):
+    soup = BeautifulSoup(html, 'lxml')
+    h2 = soup.find_all('h2')
+    h2 = soup.find_all('h2')
+    h2 = [  i.text for i in h2]
+
+    return h2
 def main():
     html = get_html(url)
     data = get_page_data(html)
+    more_data = get_more_page_data(html)
     print(data)
+    print(more_data)
 
 if __name__ == '__main__':
     main()
