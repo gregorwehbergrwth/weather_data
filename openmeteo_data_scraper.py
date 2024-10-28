cecf219a266d97a83f337e2a912e5d4d6de1263e
gregorwehbergrwth <162116009+gregorwehbergrwth@users.noreply.github.com>
2024-10-28 22:30:22 +0100

    first test

--- openmeteo_data_scraper.py
+++ openmeteo_data_scraper.py
@@ -5,19 +5,20 @@ import pandas as pd
 from retry_requests import retry
 
 # Setup the Open-Meteo API client with cache and retry on error
-cache_session = requests_cache.CachedSession('.cache', expire_after = -1)
+cache_session = requests_cache.CachedSession('.cache', expire_after = 3600)
 retry_session = retry(cache_session, retries = 5, backoff_factor = 0.2)
 openmeteo = openmeteo_requests.Client(session = retry_session)
 
 # Make sure all required weather variables are listed here
 # The order of variables in hourly or daily is important to assign them correctly below
-url = "https://archive-api.open-meteo.com/v1/archive"
+url = "https://api.open-meteo.com/v1/forecast"
 params = {
-	"latitude": 52.52,
-	"longitude": 13.41,
-	"start_date": "2024-10-12",
-	"end_date": "2024-10-26",
-	"hourly": "temperature_2m"
+	"latitude": 50.7766,
+	"longitude": 6.0834,
+	"hourly": ["temperature_2m", "relative_humidity_2m", "soil_temperature_54cm", "shortwave_radiation", "direct_radiation", "diffuse_radiation", "direct_normal_irradiance", "global_tilted_irradiance", "terrestrial_radiation"],
+	"timezone": "Europe/Berlin",
+	"forecast_days": 8,
+	"models": ["icon_seamless", "icon_global", "icon_eu", "icon_d2"]
 }
 responses = openmeteo.weather_api(url, params=params)
 
@@ -31,6 +32,14 @@ print(f"Timezone difference to GMT+0 {response.UtcOffsetSeconds()} s")
 # Process hourly data. The order of variables needs to be the same as requested.
 hourly = response.Hourly()
 hourly_temperature_2m = hourly.Variables(0).ValuesAsNumpy()
+hourly_relative_humidity_2m = hourly.Variables(1).ValuesAsNumpy()
+hourly_soil_temperature_54cm = hourly.Variables(2).ValuesAsNumpy()
+hourly_shortwave_radiation = hourly.Variables(3).ValuesAsNumpy()
+hourly_direct_radiation = hourly.Variables(4).ValuesAsNumpy()
+hourly_diffuse_radiation = hourly.Variables(5).ValuesAsNumpy()
+hourly_direct_normal_irradiance = hourly.Variables(6).ValuesAsNumpy()
+hourly_global_tilted_irradiance = hourly.Variables(7).ValuesAsNumpy()
+hourly_terrestrial_radiation = hourly.Variables(8).ValuesAsNumpy()
 
 hourly_data = {"date": pd.date_range(
 	start = pd.to_datetime(hourly.Time(), unit = "s", utc = True),
@@ -39,8 +48,17 @@ hourly_data = {"date": pd.date_range(
 	inclusive = "left"
 )}
 hourly_data["temperature_2m"] = hourly_temperature_2m
+hourly_data["relative_humidity_2m"] = hourly_relative_humidity_2m
+hourly_data["soil_temperature_54cm"] = hourly_soil_temperature_54cm
+hourly_data["shortwave_radiation"] = hourly_shortwave_radiation
+hourly_data["direct_radiation"] = hourly_direct_radiation
+hourly_data["diffuse_radiation"] = hourly_diffuse_radiation
+hourly_data["direct_normal_irradiance"] = hourly_direct_normal_irradiance
+hourly_data["global_tilted_irradiance"] = hourly_global_tilted_irradiance
+hourly_data["terrestrial_radiation"] = hourly_terrestrial_radiation
 
 hourly_dataframe = pd.DataFrame(data = hourly_data)
+# print(hourly_dataframe)
 hourly_dataframe.to_csv('scraper_output.csv', index=False)
 # print(hourly_dataframe)
 # to string
