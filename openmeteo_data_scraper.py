13a0bbeac331e8d9f1626f4f236ae3258a575296
gregorwehbergrwth <162116009+gregorwehbergrwth@users.noreply.github.com>
2024-10-28 21:02:08 +0100

    first test

--- openmeteo_data_scraper.py
+++ openmeteo_data_scraper.py
@@ -41,4 +41,6 @@ hourly_data = {"date": pd.date_range(
 hourly_data["temperature_2m"] = hourly_temperature_2m
 
 hourly_dataframe = pd.DataFrame(data = hourly_data)
-print(hourly_dataframe)
+# print(hourly_dataframe)
+# to string
+print(hourly_dataframe.to_string())
